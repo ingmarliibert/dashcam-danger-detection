3c03ad9344fcd10889b4dbf2542bf95296202c69
Maxime Moreau <contact@maxime-moreau.fr>
2020-10-22 16:12:10 +0300

    refactor: separation of concerns

--- work.py
+++ work.py
@@ -1,13 +1,12 @@
 import json
 from collections import defaultdict, namedtuple
 from dataclasses import dataclass
-from itertools import combinations
-from typing import Dict, List
+from itertools import combinations, chain
+from typing import Dict, List, Callable
 import numpy as np
 from object_detection.utils import label_map_util
 
 from utils.rectangle import Rectangle
-
 
 @dataclass
 class DetectedObject:
@@ -17,38 +16,52 @@ class DetectedObject:
     detection_boxes: list
 
 
-def is_there_any_collision(objects: List[DetectedObject]):
-    object_combinations = combinations(objects, 2)
+@dataclass
+class DetectedObjects:
+    # label name -> List
+    detected_objects: Dict[str, List[DetectedObject]]
 
-    """
-    Algorithm: loop over boxes
-    -> you have one box, you have to test if it overlaps with any other box, so you loop over all boxes but not over the current one.
-    -> you make rectangles from boxes and test if it overlaps.
-    """
-    for current_object, another_object in object_combinations:
-        person_box = current_object.detection_boxes
-        other_person_box = another_object.detection_boxes
+    def compare_specific_objects(self, label_name: str, compare: Callable[[DetectedObject, DetectedObject], any]):
+        specific_objects = self.detected_objects.get(label_name)
+        self._compare_objects(specific_objects, compare)
 
-        object_r = Rectangle(person_box[0], person_box[1], person_box[2], person_box[3])
-        other_person_r = Rectangle(other_person_box[0], other_person_box[1], other_person_box[2], other_person_box[3])
 
-        # person_r_area = rect_area(object_r)
-        # col_threshold = person_r_area
+    @staticmethod
+    def _compare_objects(to_compare: List[DetectedObject], compare: Callable[[DetectedObject, DetectedObject], any]):
+        """
+        Algorithm: loop over boxes
+        -> you have one box, you have to test if it overlaps with any other box, so you loop over all boxes but not over the current one.
+        -> you make rectangles from boxes and test if it overlaps.
+        """
+        object_combinations = combinations(to_compare, 2)
 
-        # area_between = area(object_r, other_person_r)
-        # is_collision = (area_between and area_between > col_threshold)
+        for current_object, to_compare in object_combinations:
+            compare(current_object, to_compare)
 
-        is_intersect = object_r.is_intersect(other_person_r)
 
-        if is_intersect:
-            intersection = object_r & other_person_r
-            print(f'intersection = {intersection}')
+    def compare_objects(self, compare: Callable[[DetectedObject, DetectedObject], any]):
+        all_objects = list(chain.from_iterable(self.detected_objects.values()))
+        self._compare_objects(all_objects, compare)
 
-            threshold = 0.06 * object_r.area
 
-            # print(f'{threshold} {intersection.area}')
-            if intersection.area > threshold:
-                print('collision')
+def is_there_any_collision(current_object: DetectedObject, another_object: DetectedObject):
+    person_box = current_object.detection_boxes
+    other_person_box = another_object.detection_boxes
+
+    object_r = Rectangle(person_box[0], person_box[1], person_box[2], person_box[3])
+    other_person_r = Rectangle(other_person_box[0], other_person_box[1], other_person_box[2], other_person_box[3])
+
+    is_intersect = object_r.is_intersect(other_person_r)
+
+    if is_intersect:
+        intersection = object_r & other_person_r
+        print(f'intersection = {intersection}')
+
+        threshold = 0.06 * object_r.area
+
+        # print(f'{threshold} {intersection.area}')
+        if intersection.area > threshold:
+            print('collision')
 
 
 with open('car-crash.jpg.json', 'r') as f:
@@ -64,7 +77,7 @@ with open('car-crash.jpg.json', 'r') as f:
 
     detected_objects: Dict[str, List[DetectedObject]] = defaultdict(list)
 
-    for class_id, score, box in zip(classes, scores, boxes) :
+    for class_id, score, box in zip(classes, scores, boxes):
         if score < min_score_thresh:
             continue
 
@@ -72,6 +85,7 @@ with open('car-crash.jpg.json', 'r') as f:
         detected = DetectedObject(class_name, class_id, score, box)
         detected_objects[class_name].append(detected)
 
-    persons: List[DetectedObject] = detected_objects.get('car')
+    objects = DetectedObjects(detected_objects)
+    objects.compare_specific_objects('car', is_there_any_collision)
+    # persons: List[DetectedObject] = detected_objects.get('car')
 
-    is_there_any_collision(persons)
