eb21f5efca28ecf1f78950f79f17c02aaa48b853
ingmarliibert <ingmar.liibert@gmail.com>
2020-11-17 20:55:55 +0200

    #7 Distance detection

---
+++ model/distance_detection.py
@@ -0,0 +1,10 @@
+from typing import Dict, List
+
+from model.object_detection import DetectedObject
+from utils.rectangle import Rectangle
+
+def filter(frame, detections: Dict[str, List[DetectedObject]]):
+    pass
+
+def get_distance(frame, car_bounding_box: Rectangle):
+    pass
